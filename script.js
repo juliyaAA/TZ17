49b30f4a206ca84421b9cac6a6515fd59c20a61a
juliyaAA <yulya919@yandex.ru>
2020-09-11 15:32:52 +0300

    17-2.

--- script.js
+++ script.js
@@ -1,40 +1,35 @@
 // Задача 1. Создайте функцию, которая возвращает ваше имя. Имя вводится с input
-// function getInputName (){
-    
-//     return(pow.value);
-// }
-// getInputName ();
+// let button = document.getElementById('button');
 
-button.onclick = function() {
-    const pow = document.getElementById('userName').value;
-    document.getElementById('str').innerHTML="Ваше имя: " + pow;
-    return false;
-};
+let pow = document.getElementById('userName');
+function buttonСlick() {
+    document.getElementById('str').innerHTML="Ваше имя: " + pow.value;
+}
+buttonСlick();
 
 // Задача 2 и 3. Создайте функцию, которая принимает 2 числа и возвращает большее число из двух, либо любое из них если они равны.
+// 1й способ 
 
- 
-function getComparison (a, b) {
-    if ( a > b ) {
+function getComparison(a, b) {
+    if ((a > b) || (a == b)) {
         return a;
-    } else if( a == b ) {
-            return a || b ;
-        }
+    }
 }
-getComparison (9, 7); 
+getComparison(9, 7);
+
+// 2й способ
+function getEvent (c, d) {
+    return Math.max(c , d);
+}
+getEvent (10, 7); 
 
 /* Задача 4. Создайте функцию, которая возвращает случайное число в указанном
 диапазоне. Диапазон указывает пользователь с помощью двух переменных, которые
 передаются функции в качестве параметров. */
-// function getRandom(c, d) {
-//     c = Math.ceil(c);
-//     d = Math.floor(d);
-//     return Math.floor(Math.random() * (d - c)) + min;;
-//   }
-// getRandom (1, 20);
+
 function getRandom(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-  }
-  getRandom (1, 20);
+    let min1 = Math.ceil(min);
+    let max2 = Math.floor(max);
+    return Math.floor(Math.random() * (max2 - min1 + 1)) + min1; 
+}
+getRandom (1, 20);
